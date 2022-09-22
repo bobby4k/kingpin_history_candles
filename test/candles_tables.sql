7c81fbc56ec2987d0a2a8c0888c1f91a69f05fb3
Bobby Jin <bobby4kit@outlook.com>
2022-09-22 12:33:02 +0800

    tmp commit

--- test/candles_tables.sql
+++ test/candles_tables.sql
@@ -7,4 +7,11 @@ CREATE TABLE 'candles_btc-usdt' (
     volume float
 ) TIMESTAMP (timestamp) PARTITION by DAY;
 
+-- INSERT INTO 'candles_btc-usdt' VALUES(1630425660000000, 48772.2, 48801.4, 48714.6, 48735.5, 1009.0);
+SELECT * from 'candles_btc-usdt'
+
+-- INSERT INTO 'candles_btc-usdt' VALUES(1663813260000000, 18527.7, 18527.7, 18518.7, 18519.6, 70.0);
+-- TRUNCATE TABLE 'candles_btc-usdt';
+
+
 
